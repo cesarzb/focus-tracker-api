24695cb5f2d1ddc1870b4144bed86e438b3c16c1
Bernard Cesarz <cesarzb@protonmail.com>
2026-01-15 23:06:28 +0100

    Added sessions module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { SessionModule } from './sessions/sessions.module';
 
 @Module({
   imports: [
     TasksModule,
+    SessionModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
