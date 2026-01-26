ebe8ffdec462acf8e2f2056fcdee543b1799ee61
cesarzb <cesarzb@protonmail.com>
2026-01-26 16:24:13 +0100

    Added environment variables config for db

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,20 +4,36 @@ import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { SessionsModule } from './sessions/sessions.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     TasksModule,
     SessionsModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'focus_tracker_db',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        const databaseUrl = config.get<string>('DATABASE_URL');
+
+        return {
+          type: 'postgres',
+          url: databaseUrl,
+          host: !databaseUrl ? config.get<string>('DB_HOST') : undefined,
+          port: !databaseUrl ? config.get<number>('DB_PORT') : undefined,
+          username: !databaseUrl
+            ? config.get<string>('DB_USERNAME')
+            : undefined,
+          password: !databaseUrl
+            ? config.get<string>('DB_PASSWORD')
+            : undefined,
+          database: !databaseUrl ? config.get<string>('DB_NAME') : undefined,
+          entities: [__dirname + '/**/*.entity{.ts,.js}'],
+          synchronize: config.get('NODE_ENV') !== 'production',
+
+          ssl: databaseUrl ? { rejectUnauthorized: false } : false,
+        };
+      },
     }),
   ],
   controllers: [AppController],
