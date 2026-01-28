65008cb2aaf86dfc3d77e652f41ffd8dd883fb83
cesarzb <cesarzb@protonmail.com>
2026-01-28 14:36:32 +0100

    Feature: Auth - user can login

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,32 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from 'src/users/users.service';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  async signIn(
+    username: string,
+    pass: string,
+  ): Promise<{ access_token: string }> {
+    const user =
+      await this.usersService.findOneByUsernameWithPassword(username);
+
+    const isMatch = await bcrypt.compare(pass, user?.password);
+
+    if (!isMatch) {
+      throw new UnauthorizedException();
+    }
+
+    const payload = { sub: user.id, username: user.username };
+
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
+  }
+}
