bbaf1f5ae1f4cea4fcf11572132de7c74a409154
cesarzb <cesarzb@protonmail.com>
2026-02-10 15:21:32 +0100

    Added relations between tasks, sessions and user

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,5 +1,7 @@
 import { Exclude } from 'class-transformer';
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
+import { Task } from 'src/tasks/entities/task.entity';
+import { Session } from 'src/sessions/entities/session.entity';
 
 @Entity()
 export class User {
@@ -12,4 +14,10 @@ export class User {
   @Column({ select: false })
   @Exclude({ toPlainOnly: true })
   password: string;
+
+  @OneToMany(() => Task, (task) => task.user)
+  tasks: Task[];
+
+  @OneToMany(() => Session, (session) => session.user)
+  sessions: Session[];
 }
