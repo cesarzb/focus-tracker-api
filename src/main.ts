f752c291f79f6bd5f11df1eaa8f89ab5eb42e40a
cesarzb <cesarzb@protonmail.com>
2026-02-20 15:33:17 +0100

    Added CORS config to accept connections from local frontend

--- src/main.ts
+++ src/main.ts
@@ -34,6 +34,14 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
 
   SwaggerModule.setup('api', app, document);
+
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
